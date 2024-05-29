133e773d770cf4f6746323dc7330c9687ce3c6e8
MufassherKhan <mp4mp4k@gmail.com>
2024-05-29 23:59:56 +0600

    intial commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'XM Week 3',
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+    );
+  }
+}
